ae0854a964aad7eadd663c7eaa6369e8c87e0607
wiem-2001 <wiembm2001@gmail.com>
2025-10-27 00:46:19 +0100

    remove unecessary file

--- gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-import multiprocessing
-
-# Réduire le nombre de workers pour économiser la mémoire
-workers = 1
-worker_class = "uvicorn.workers.UvicornWorker"
-bind = "0.0.0.0:10000"
-timeout = 120
